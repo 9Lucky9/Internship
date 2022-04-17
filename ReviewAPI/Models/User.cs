13432f4a416c40f0fd4940a20da62604706c1623
Павел Евстигнеев <pro1912@gmail.com>
2022-04-17 07:53:27 +0300

    Добавление модели пользователя

---
+++ ReviewAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace ReviewAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        private User() { }
+
+        public User(int id, string login, string password)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+        }
+    }
+}
