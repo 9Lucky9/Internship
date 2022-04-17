f1e694280228864e880d60df8dc26da2fc49372d
Павел Евстигнеев <pro1912@gmail.com>
2022-04-17 08:52:27 +0300

    Добавление репозитория для работы с таблицой Users

---
+++ ReviewAPI/Repository/IUser.cs
@@ -0,0 +1,10 @@
+﻿using ReviewAPI.Models;
+
+namespace ReviewAPI.Repository
+{
+    public interface IUser
+    {
+        public User FindUser(string login, string password);
+        public User GetUser(int id);
+    }
+}
