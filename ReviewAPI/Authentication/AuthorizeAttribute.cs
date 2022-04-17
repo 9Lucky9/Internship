aa395540b7f23064bc5abd320f3e7efaa132ec48
Павел Евстигнеев <pro1912@gmail.com>
2022-04-17 08:55:39 +0300

    Атрибут для кастомной авторизации

---
+++ ReviewAPI/Authentication/AuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ReviewAPI.Models;
+
+namespace ReviewAPI.Authentication
+{
+    /// <summary>
+    /// Атрибут применяемый для авторизации
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+            if (allowAnonymous)
+                return;
+
+            var user = (User)context.HttpContext.Items["User"];
+            if (user == null)
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+    }
+}
