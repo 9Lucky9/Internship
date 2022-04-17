cf8e59dd003a9376ed86f35b081b0abd704280a6
Павел Евстигнеев <pro1912@gmail.com>
2022-04-17 08:58:30 +0300

    Класс реализующий middleware расшифровку токена

---
+++ ReviewAPI/TokenMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using ReviewAPI.Authentication;
+
+namespace ReviewAPI
+{
+    public class TokenMiddleware
+    {
+        private RequestDelegate _next;
+
+        public TokenMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context, IAuthentication Authentication)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var userId = TokenHelper.ValidateToken(token);
+            if (userId != null)
+            {
+                context.Items["User"] = Authentication.GetUser((int)userId);
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class TokenMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseTokenMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TokenMiddleware>();
+        }
+    }
+}
